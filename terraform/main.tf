de928f6ff868910c304ff3e9aae23047ea2fae20
Sampo Ahokas <sampo@sema4.ai>
2025-03-11 01:13:18 +0200

    terraform: update example EventBridge source rules

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ module "eventbridge" {
   id     = var.id
   eventbridge_external_in_allowed_source_rule_arns = [
     // Update this according to instructions here: https://robocorp.com/docs/control-room/aws-integrations/eventbridge#configuration
-    "arn:aws:events:eu-west-1:827142569422:rule/demo-customer-integration/org-0f9993a1-92e4-4b4d-8ff7-07f1ea39dfaf",
+    "arn:aws:events:eu-west-1:827142569422:rule/ci-customer-integration/org-17cb325d-2278-4fd0-a8f3-b28149ccfa64",
     "arn:aws:events:us-east-1:312661337933:rule/us1-customer-integration/org-2db51ec1-a021-4142-a3af-3e1b46ebc7c7",
   ]
 }
